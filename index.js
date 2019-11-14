755363fda149e87f9d31698a00a7041bb5caa7e9
taufik66645 <taufik.66645@gmail.com>
2019-11-14 13:01:28 +0700

    modifying file

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const chalk = require("chalk");
 const dcNames = require("dc-names");
 const moment = require('moment');
 const date  = moment().format('MMMM Do YYYY, h:mm:ss a');
+const day = moment().format('dddd');
+
 
 
 
 console.log(
   cowsay.say({
-    text: "Hello",
+    text: "Hello my name is Taufik",
     e: "oO",
     T: "U "
   })
 );
 
-console.log(chalk.red.bgYellow.bold('hahahaha'));
+console.log(chalk.red.bgYellow.bold('This is chalk example'));
 console.log(dcNames);
 console.log(date);
-
+console.log(day);
 
 
 
